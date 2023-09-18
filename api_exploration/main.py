d6e2892e7e12fc4b398cd900e1055f6bba4c5623
Max Väistö <maxvaist@ad.helsinki.fi>
2023-09-18 09:02:58 +0300

    Added API reading capabilities
    API information now read from SteamSpy and Steam for each individual
    game. Only data seen as relevant is gathered from the APIs

--- api_exploration/main.py
+++ api_exploration/main.py
@@ -1,4 +1,6 @@
 import itertools
+import datetime
+from concurrent.futures import ThreadPoolExecutor
 
 import requests
 import json
@@ -7,15 +9,26 @@ import time
 import matplotlib.pyplot as plt
 from pandas.core.dtypes.common import is_numeric_dtype
 
-sisu = "https://steamspy.com/api.php?request=all&page="
+STEAMSPY_ALL_GAMES_URL = "https://steamspy.com/api.php?request=all&page="
+STEAM_GAME_INFO_URL = "https://store.steampowered.com/api/appdetails?appids="
+STEAM_API_LANGUAGE = "&l=english"
+STEAM_SPY_GAME_INFO = "https://steamspy.com/api.php?request=appdetails&appid="
 
 
 # There are 67 pages of data but for the heck of it,
 # we're going try to load 100 pages
-def get_all_data(iterations: int = 100):
+def get_all_data(iterations: int = 100, num_threads: int = 4):
+    def get_api_data_for_game_threaded(id):
+        steam_api_data = get_additional_game_data_steam(str(id))
+        steamspy_api_data = get_additional_game_data_steamspy(str(id))
+        return steam_api_data, steamspy_api_data
+    # def get_additional_game_data_steam_threaded(id):
+    #     return get_additional_game_data_steam(str(id))
+    # def get_additional_game_data_steamspy_threaded(id):
+    #     return get_additional_game_data_steamspy(str(id))
     for i in range(iterations):
         print(i)
-        url = sisu + str(i)
+        url = STEAMSPY_ALL_GAMES_URL + str(i)
         try:
             response = json.loads(requests.get(url).text)
         except Exception as some_shit:
@@ -25,10 +38,66 @@ def get_all_data(iterations: int = 100):
         if i == 0:
             df = pandas.DataFrame(games)
         else:
-            df_comb = pandas.DataFrame(games)
-            df = pandas.concat([df, df_comb], ignore_index=True, sort=False)
-        time.sleep(0)
+            df = pandas.concat([df, pandas.DataFrame(games)], ignore_index=True, sort=False)
+
+    # df = df.iloc[0:10]
+    # with ThreadPoolExecutor(max_workers=num_threads) as executor:
+    #     steam_results = list(executor.map(get_additional_game_data_steam_threaded, df["appid"]))
+    #
+    # with ThreadPoolExecutor(max_workers=num_threads) as executor:
+    #     steamspy_results = list(executor.map(get_additional_game_data_steamspy_threaded, df["appid"]))
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        combined_results = list(executor.map(get_api_data_for_game_threaded, df["appid"]))
+
+    steam_results = [result[0] for result in combined_results]
+    steamspy_results = [result[1] for result in combined_results]
+
+    df = pandas.concat([df, pandas.DataFrame(steam_results), pandas.DataFrame(steamspy_results)], axis=1)
+
+    # steam_api_data = df["appid"].iloc[:].apply(lambda x: get_additional_game_data_steam(str(x)))
+    # df = pandas.concat([df, steam_api_data], axis=1)
     return df
+
+
+def get_additional_game_data_steam(id):
+    url = STEAM_GAME_INFO_URL + id + STEAM_API_LANGUAGE
+
+    response = json.loads(requests.get(url).text)
+    print(datetime.datetime.now())
+    fails = 0
+    base_wait = 60
+    while not response or response[id]["success"] == False:
+        # This part is meant to catch
+        if response and response[id]["success"] == False:
+            fails += 1
+            if fails >= 10:
+                return pandas.Series({'platforms': [], 'release_date': "", 'categories': []})
+        print("Failed queries for", id, "is", fails)
+        time.sleep(base_wait)
+        response = json.loads(requests.get(url).text)
+
+
+    data = response[id]["data"]
+    if data["type"] != "game":
+        print("Non game found", data["name"])
+    platforms = [platform for (platform, enabled) in data["platforms"].items() if enabled]
+    release_date = data["release_date"]["date"]
+    categories = [category_data["description"] for category_data in data["categories"]] if "categories" in data.keys() else []
+    return_values = pandas.Series({'platforms': platforms, 'release_date': release_date, 'categories': categories})
+    return return_values
+
+
+def get_additional_game_data_steamspy(id):
+    url = STEAM_SPY_GAME_INFO + id
+    response = json.loads(requests.get(url).text)
+    # tags = [tag for (tag,tag_id) in response["tags"]]
+    languages = response["languages"].split(", ")
+    genres = response["genre"]
+    ccu = response["ccu"]
+    tags = [tag for (tag, tag_id) in response["tags"].items()] if response["tags"] else []
+
+    return_values = dict(ccu=ccu, languages=languages, genres=genres, tags=tags)
+    return return_values
 
 
 def add_user_rating(df):
@@ -48,7 +117,7 @@ def create_hist_plots(df):
             plt.hist(df[col_name], log=True)
             title = " ".join([col_name, "log histogram"])
             plt.title(title)
-            fig.savefig("".join(["images\\",title,".png"]))
+            fig.savefig("".join(["images\\", title, ".png"]))
             plt.show()
 
 
@@ -74,16 +143,16 @@ def create_heat_maps(df, plot_pairs):
 
 
 def price_to_dollars(convert_df):
-    convert_df["price"] = convert_df["price"].apply(lambda val: int(val)/100 if int(val) != 0 else 0)
+    convert_df["price"] = convert_df["price"].apply(lambda val: int(val) / 100 if int(val) != 0 else 0)
     return convert_df
 
 
 if __name__ == "__main__":
-    df = get_all_data(5)
+    df = get_all_data()
     df = add_user_rating(df)
     df = replace_owner_number_with_symbol(df)
     df = price_to_dollars(df)
-
+    df.to_csv("game_data_experimental.csv")
     h = df.describe()
     j = df.isna().sum()
     c = df["userscore"].value_counts()
@@ -100,5 +169,5 @@ if __name__ == "__main__":
     plt.title("Histogram of game playerbase sizes with log scale")
     plt.tight_layout()
     plt.show()
-    df.to_csv("game_data.csv")
+    df.to_csv("game_data_full.csv")
     pass
